d62fa02afd8f85130f123bf4dcb6746b35181302
iphycodes <ifeanyiemmanuel585@gmail.com>
2025-07-23 18:00:19 +0100

    feat: completed all

--- src/app/(ui)/(dashboard)/page.tsx
+++
@@ -1,8 +0,0 @@
-'use cleint';
-import React from 'react';
-
-const DashboardPage = () => {
-  return <div className="monsterrat">This is the dashboard page</div>;
-};
-
-export default DashboardPage;
